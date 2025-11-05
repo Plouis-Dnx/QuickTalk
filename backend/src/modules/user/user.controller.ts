570cf1ec15eca3afc16d2403373ac792af83db0b
Plouis-Dnx <plouis.deneux@gmail.com>
2025-11-05 23:01:33 +0100

    feat: add /users route

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -2,13 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
-  @Get('hello')
-  getHello(): string {
-    return 'Hello Users!';
-  }
-
-  @Get('goodbye')
-  getGoodbye(): string {
-    return 'Goodbye Users!'
+  @Get()
+  getUsers(): string {
+    return 'Return all the users';
   }
 }
