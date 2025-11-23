2fbb6a1f81e31078871e74ce58d1adb99af15ae6
Plouis-Dnx <plouis.deneux@gmail.com>
2025-11-23 20:48:37 +0100

    chore: merged users and auth

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './user.schema';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -45,4 +44,8 @@ export class UserService {
         if (!deletedUser) throw new NotFoundException(`user not found`);
         return deletedUser;
     }
+}
+
+function InjectModel(name: string): (target: typeof UserService, propertyKey: undefined, parameterIndex: 0) => void {
+  throw new Error('Function not implemented.');
 }
