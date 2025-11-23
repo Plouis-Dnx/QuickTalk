d620da986afc00f6e551fe9a2ae4a5fc22ca712e
Plouis-Dnx <plouis.deneux@gmail.com>
2025-11-23 18:29:02 +0100

    feat(user): add getUserByEmail method

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -14,7 +14,13 @@ export class UserService {
 
   async getUserById(id: string): Promise<User> {
     const user = await this.userModel.findById(id).exec();
-    if(!user) throw new NotFoundException(`user not found`)
+    if(!user) throw new NotFoundException('user not found')
+    return user;
+  }
+
+  async getUserByEmail(email: string): Promise<User> {
+    const user = await this.userModel.findOne({email}).exec();
+    if(!user) throw new NotFoundException('user not found');
     return user;
   }
 
