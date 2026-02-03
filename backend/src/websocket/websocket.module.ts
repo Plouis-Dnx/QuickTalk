1a71bb2680f22725d23ca0a8a8ffea90951343e7
WildRock <plouis.deneux@gmail.com>
2026-02-03 13:19:18 +0100

    feat(ws): add send-message dto

--- backend/src/websocket/websocket.module.ts
+++ backend/src/websocket/websocket.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './gateways/events.gateway';
+import { AuthModule } from 'src/domain/auth/auth.module';
+import { UserModule } from 'src/domain/user/user.module';
 
 @Module({
+    imports: [
+        AuthModule,
+        UserModule
+    ],
     providers: [
         EventsGateway
     ]
